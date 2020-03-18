9d129720ad0e6e98c3a1e655dd662edf2d9ed558
Saggod <kkthxkk@gmail.com>
2020-03-19 00:37:18 +0300

    +2 задания
    1,2 и 1,3

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,4 +2,22 @@ public class MainClass {
     public int getLocalNumber() {
         return 14;
     }
+
+    private int class_number = 20;
+
+    public int gotClassNumber() {
+        return class_number;
+    }
+
+    private String class_string() {
+        String a = "hello, world";
+        return a;
+    }
+
+    public String getClassString(){
+       return class_string();
+
+    }
+
+
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,13 +1,40 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class MainClassTest extends MainClass {
 
     @Test
     public void testGetLocalNumber() {
-
         this.getLocalNumber();
         Assert.assertTrue("getLocalNumber = 14", getLocalNumber() == 14);
     }
 
+    @Test
+    public void testGetClassNumber(){
+        int a = this.gotClassNumber();
+        if (a > 45){
+            System.out.println("True");
+        } else {
+            System.out.println("Fail");
+        }
+
+    }
+
+
+    @Test
+    public void testGetClassString() {
+        String a = this.getClassString();
+        String firstWord = a.substring(0, 5);
+
+
+        if (firstWord.contains("Hello")){
+            System.out.println("В тексте первое слово \"Hello\"");
+        } else if (firstWord.contains("hello")) {
+            System.out.println("В тексте первое слово \"hello\"");
+        } else {
+            Assert.assertTrue("Первое слово отличается от \"Hello\" или \"hello\"",  firstWord == "hello" ^ firstWord =="Hello");
+        }
+
+    }
 }
